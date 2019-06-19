93150faf558a4941a6aac60a72b3310a5283373b
Kelton Karboviak <kelton.karboviak@gmail.com>
2019-06-19 05:19:19 +0100

    Completed Part 5 of the Mix & OTP tutorial on elixir-lang.org

--- kv/test/kv/supervisor_test.exs
+++ kv/test/kv/supervisor_test.exs
@@ -1,7 +1,3 @@
 defmodule KV.SupervisorTest do
   use ExUnit.Case, async: true
-
-  test "supervises single KV.Registry instance" do
-    [{_, _registry, _, _}] = Supervisor.which_children(KV.Supervisor)
-  end
 end
